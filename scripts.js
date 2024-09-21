5f8e899de20e06816d50aa2abc0fb31336c4a732
Theo <sagniez.theo@laposte.net>
2024-09-21 12:37:23 +0200

    added submit answers button + functionality to check if user answers are correct

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
 const selectEls = document.getElementsByName('q')
-const answers = {}
+const answerButton = document.querySelector('#submitAnswers')
+const userAnswers = {}
+const answers = {
+    'q1': '2',
+    'q2': '4',
+    'q3': '3'
+}
 
 selectEls.forEach(el => {
-    answers[el.id] = null
+    userAnswers[el.id] = null
 
     el.addEventListener('change', () => {
-        answers[el.id] = el.options[el.selectedIndex].value
-        console.log(answers)    
+        userAnswers[el.id] = el.options[el.selectedIndex].value
     })
+})
+
+answerButton.addEventListener('click', () => {
+    for (let answer in answers) {
+        if (userAnswers[answer] !== answers[answer]) {
+            console.log('mistake found')
+
+            return
+        }
+    }
+
+    console.log('no mistakes found')
 })
