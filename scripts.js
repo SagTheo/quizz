dc5de87e9f7b5a0f64bba0a99080dec6dab0bf0d
Theo <sagniez.theo@laposte.net>
2024-09-28 12:04:12 +0200

    added mistakes counter for quizz v2

--- scripts.js
+++ scripts.js
@@ -18,6 +18,7 @@ const answersV2 = {
     'q3_V2': '4'
 }
 let mistakesCounter = 0
+let mistakesCounterV2 = 0
 let errSign = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/></svg>'
 
 selectEls.forEach(el => {
@@ -51,6 +52,8 @@ answerButtonQ1.addEventListener('click', () => {
 
 // Quizz v2
 answerButtonQ2.addEventListener('click', () => {
+    mistakesCounterV2 = 0
+
     for (let i = 0; i < arrAnswers.length; i++) {
         const arrInputs = arrAnswers[i].elements
         const id = arrAnswers[i].id
@@ -58,9 +61,15 @@ answerButtonQ2.addEventListener('click', () => {
         for (let j = 0; j < arrInputs.length; j++) {
             if (arrInputs[j].checked) {
                 if (arrInputs[j].defaultValue !== answersV2[id]) {
-                    console.log('wrong answer')
+                    mistakesCounterV2++
                 }
             }
         }
+    }
+
+    if (mistakesCounterV2 === 0 || mistakesCounterV2 > 1) {
+        resultV2.innerHTML = `You made ${mistakesCounterV2} mistakes`
+    } else {
+        resultV2.innerHTML = `You made ${mistakesCounterV2} mistake`
     }
 })
