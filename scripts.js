888a95ba7f3c2713876548f44789623fbf5f58e6
Theo <sagniez.theo@laposte.net>
2024-09-29 12:00:37 +0200

    worked on controlling that user answers all the questions

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const result = document.querySelector('.result')
 const resultV2 = document.querySelector('.resultV2')
 const userAnswers = {}
 const userAnswersV2 = {}
+let hasAnswered = false
 const answers = {
     'q1': '2',
     'q2': '4',
@@ -21,31 +22,43 @@ let mistakesCounter = 0
 let mistakesCounterV2 = 0
 let errSign = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/></svg>'
 
+// needs more work
 selectEls.forEach(el => {
     el.addEventListener('change', () => {
-        userAnswers[el.id] = el.options[el.selectedIndex].value
+        if (el.options[el.selectedIndex].value !== 'title') {
+            hasAnswered = true
+            
+            userAnswers[el.id] = el.options[el.selectedIndex].value
+        } else {
+            hasAnswered = false
+        }
     })
 })
 
 answerButtonQ1.addEventListener('click', () => {
+    console.log(userAnswers)
     mistakesCounter = 0
     
-    for (let answer in answers) {
-        const errMessage = document.querySelector(`.result_${answer}`)
+    if (hasAnswered) {
+        for (let answer in answers) {
+            const errMessage = document.querySelector(`.result_${answer}`)
 
-        if (userAnswers[answer] !== answers[answer]) {
-            errMessage.innerHTML = errSign
+            if (userAnswers[answer] !== answers[answer]) {
+                errMessage.innerHTML = errSign
 
-            mistakesCounter++
-        } else {
-            errMessage.innerHTML = ''
+                mistakesCounter++
+            } else {
+                errMessage.innerHTML = ''
+            }
         }
-    }
 
-    if (mistakesCounter === 0 || mistakesCounter > 1) {
-        result.innerHTML = `You made ${mistakesCounter} mistakes`
+        if (mistakesCounter === 0 || mistakesCounter > 1) {
+            result.innerHTML = `You made ${mistakesCounter} mistakes`
+        } else {
+            result.innerHTML = `You made ${mistakesCounter} mistake`
+        }    
     } else {
-        result.innerHTML = `You made ${mistakesCounter} mistake`
+        result.innerHTML = 'You must answer all the questions'
     }
 })
 
@@ -53,6 +66,7 @@ answerButtonQ1.addEventListener('click', () => {
 // Quizz v2
 answerButtonQ2.addEventListener('click', () => {
     mistakesCounterV2 = 0
+    let answered = 0
 
     for (let i = 0; i < arrAnswers.length; i++) {
         const arrInputs = arrAnswers[i].elements
@@ -60,6 +74,8 @@ answerButtonQ2.addEventListener('click', () => {
 
         for (let j = 0; j < arrInputs.length; j++) {
             if (arrInputs[j].checked) {
+                answered++
+
                 if (arrInputs[j].defaultValue !== answersV2[id]) {
                     mistakesCounterV2++
                 }
@@ -67,9 +83,13 @@ answerButtonQ2.addEventListener('click', () => {
         }
     }
 
-    if (mistakesCounterV2 === 0 || mistakesCounterV2 > 1) {
-        resultV2.innerHTML = `You made ${mistakesCounterV2} mistakes`
+    if (answered === arrAnswers.length) {
+        if (mistakesCounterV2 === 0 || mistakesCounterV2 > 1) {
+            resultV2.innerHTML = `You made ${mistakesCounterV2} mistakes`
+        } else {
+            resultV2.innerHTML = `You made ${mistakesCounterV2} mistake`
+        }    
     } else {
-        resultV2.innerHTML = `You made ${mistakesCounterV2} mistake`
+        resultV2.innerHTML = 'You must answer all the questions'
     }
 })
