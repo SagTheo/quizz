53bcc8bb7b63e4a59acbdaca34ef6ab96c4db6af
Theo <sagniez.theo@laposte.net>
2024-09-30 17:57:31 +0200

    kept working on controlling that user answered all the questions

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ const result = document.querySelector('.result')
 const resultV2 = document.querySelector('.resultV2')
 const userAnswers = {}
 const userAnswersV2 = {}
-let hasAnswered = false
 const answers = {
     'q1': '2',
     'q2': '4',
@@ -22,26 +21,29 @@ let mistakesCounter = 0
 let mistakesCounterV2 = 0
 let errSign = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/></svg>'
 
-// needs more work
+// needs more work: hiding error icon when going from wrong answer to Choose an answer
 selectEls.forEach(el => {
     el.addEventListener('change', () => {
-        if (el.options[el.selectedIndex].value !== 'title') {
-            hasAnswered = true
-            
-            userAnswers[el.id] = el.options[el.selectedIndex].value
-        } else {
-            hasAnswered = false
-        }
+        userAnswers[el.id] = el.options[el.selectedIndex].value
     })
 })
 
 answerButtonQ1.addEventListener('click', () => {
     console.log(userAnswers)
     mistakesCounter = 0
+    let answersCounter = 0
+
+    for (let answer in answers) {
+        const errMessage = document.querySelector(`.result_${answer}`)
+
+        if (userAnswers[answer]) {
+            if (userAnswers[answer] === 'title') {
+                result.innerHTML = 'You must answer all the questions'
     
-    if (hasAnswered) {
-        for (let answer in answers) {
-            const errMessage = document.querySelector(`.result_${answer}`)
+                return 
+            }
+
+            answersCounter++
 
             if (userAnswers[answer] !== answers[answer]) {
                 errMessage.innerHTML = errSign
@@ -50,16 +52,16 @@ answerButtonQ1.addEventListener('click', () => {
             } else {
                 errMessage.innerHTML = ''
             }
-        }
-
-        if (mistakesCounter === 0 || mistakesCounter > 1) {
-            result.innerHTML = `You made ${mistakesCounter} mistakes`
-        } else {
-            result.innerHTML = `You made ${mistakesCounter} mistake`
-        }    
-    } else {
-        result.innerHTML = 'You must answer all the questions'
+        }  
     }
+
+    if (answersCounter !== 3) {
+        result.innerHTML = 'You must answer all the questions'
+    } else if (mistakesCounter === 0 || mistakesCounter > 1) {
+        result.innerHTML = `You made ${mistakesCounter} mistakes`
+    } else {
+        result.innerHTML = `You made ${mistakesCounter} mistake`
+    }  
 })
 
 
