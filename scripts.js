0e78125c0916a9b08df5df587578ed31d6c437fc
Theo <sagniez.theo@laposte.net>
2024-10-03 20:08:28 +0200

    added red border when user picks wrong answer in second quizz

--- scripts.js
+++ scripts.js
@@ -26,7 +26,6 @@ let mistakesCounter = 0
 let mistakesCounterV2 = 0
 let errSign = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/></svg>'
 
-// needs more work -> hide all dsiplayed error signs when user switches an answer back to Choose an answer
 selectEls.forEach(el => {
     el.addEventListener('change', () => {
         userAnswers[el.id] = el.options[el.selectedIndex].value
@@ -80,12 +79,18 @@ answerButtonQ2.addEventListener('click', () => {
         const arrInputs = arrAnswers[i].elements
         const id = arrAnswers[i].id
 
+        if (arrAnswers[i].classList.contains('wrong')) {
+            arrAnswers[i].classList.remove('wrong')
+        } 
+
         for (let j = 0; j < arrInputs.length; j++) {
             if (arrInputs[j].checked) {
                 answered++
 
                 if (arrInputs[j].defaultValue !== answersV2[id]) {
                     mistakesCounterV2++
+
+                    arrAnswers[i].classList.add('wrong')
                 }
             }
         }
